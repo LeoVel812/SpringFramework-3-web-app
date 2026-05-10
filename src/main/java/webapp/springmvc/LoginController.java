97fa2853ee8ae109ebee2d929eeb1d1559e5ed10
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-10 02:37:24 -0600

    add a spring mvc controller, get request

---
+++ src/main/java/webapp/springmvc/LoginController.java
@@ -0,0 +1,15 @@
+package webapp.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login")
+    @ResponseBody
+    public String hello() {
+        return "Hello there!!";
+    }
+}
