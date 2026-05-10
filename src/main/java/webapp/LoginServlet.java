338ebbd25979331a6b7f999e4c821e5d64a299ab
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-09 21:02:21 -0600

    update code and deps to run the war archive

---
+++ src/main/java/webapp/LoginServlet.java
@@ -0,0 +1,29 @@
+package webapp;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>yeik!!!!!!!!</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("My First Servlet");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
