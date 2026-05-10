d9cc16f09c5d8e53c666aac9014c8c6d3111294c
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-09 21:39:02 -0600

    redirect request to a jsp

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -1,29 +1,19 @@
 package webapp;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.PrintWriter;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
         response.setContentType("text/html");
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Yeah!!!!!!</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("My First Servlet");
-            out.println("</body>");
-            out.println("</html>");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+<title>Yeah!! From JSP</title>
+</head>
+<body>
+My First JSP!!!
+</body>
+</html>
