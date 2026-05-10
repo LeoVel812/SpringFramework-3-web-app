5f6f385bb40cca728f722e8e83cede51dc05d526
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-10 00:59:49 -0600

    get parameter and pass it to the jsp

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -5,15 +5,23 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(LoginServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+        String name = request.getParameter("name");
+        log.info("Parameter name: {}", name);
+        request.setAttribute("name", name);
+
         response.setContentType("text/html");
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
     }
 
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
+<html>
 <head>
 <title>Yeah!! From JSP</title>
 </head>
 <body>
-My First JSP!!!
+My First JSP ${name}!!!
 </body>
 </html>
